45b27d30acd28514db82848715939a0e51940139
Rahul Singh <cscrahul001@gmail.com>
2021-04-26 20:14:34 +0530

    minor updates

---
+++ Nunit_Jasmine_Karma_Assignments/Nunit/Assignment8Nunit/TestService/Service/MethodClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Service
+{
+    public class MethodClass
+    {
+
+        public List<UserData> GetUserData()
+        {
+            
+            return new List<UserData>
+            {
+                new UserData {Name="Rahul", Age=21, Address= "Bihar"},
+                new UserData { Name= "Tony Stark", Age=38, Address="New York" },
+                new UserData { Name= "Dr. Steven Strange", Age=45, Address="New York" },
+                new UserData { Name= "Wanda", Age=26, Address="Soukovia" },
+                new UserData { Name= "Natasha RomanRoff", Age=28, Address="Germany" },
+                new UserData { Name= "Peter Quil", Age=40, Address="Xandar" },
+
+            };
+
+        }
+
+        
+        
+
+
+    }
+}
